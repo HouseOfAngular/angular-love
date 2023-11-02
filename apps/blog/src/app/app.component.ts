61b7972fadf63f040af5b0be5f8e00d55489f851
Mateusz Stefańczyk <mateusz.stefanczyk@valueadd.pl>
2023-11-02 22:47:36 +0100

    feat: remove nx welcome component

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { LayoutComponent } from "@angular-love/blog/layouts/ui";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, LayoutComponent],
+  imports: [LayoutComponent],
   selector: 'angular-love-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'blog';
-}
+export class AppComponent {}
