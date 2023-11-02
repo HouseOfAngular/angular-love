16fccbd738b0beb23789ab075b8ce684b0402fa5
mateusz.dobrowolski@valueadd.pl <mateusz.dobrowolski@valueadd.pl>
2023-11-02 23:52:19 +0100

    Add Storybook config

---
+++ libs/blog/layouts/ui/src/lib/layout/layout.component.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { LayoutComponent } from './layout.component';
+
+const meta: Meta<LayoutComponent> = {
+  component: LayoutComponent,
+  title: 'Layouts / Layout',
+};
+export default meta;
+type Story = StoryObj<LayoutComponent>;
+
+export const Primary: Story = {
+  args: {},
+};
