87f1137167b257c6451fcb69db14380bbb0886d1
mateusz.dobrowolski@valueadd.pl <mateusz.dobrowolski@valueadd.pl>
2023-11-03 01:59:17 +0100

    button cmp

--- libs/blog/shared/ui/button/src/lib/button/button.component.ts
+++ libs/blog/shared/ui/button/src/lib/button/button.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
+export type AlButtonVariant = 'Primary' | 'Secondary' |  'Outline';
+
+const hostClassMap: Record<AlButtonVariant, string> = {
+  Primary: 'al-button al-button--primary rounded-lg px-8 py-2',
+  Secondary: 'al-button al-button--secondary rounded-lg px-8 py-2',
+  Outline: 'al-button al-button--outline rounded-lg px-8 py-2',
+}
+
 
 @Component({
   selector: 'button[al-button],a[al-button]',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input() set variant(val: AlButtonVariant) {
+    this.hostClasses = hostClassMap[val];
+  };
+
+  @HostBinding('class')
+  hostClasses = '';
+}
