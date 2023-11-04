39257d597275e456745eadf4a360509e193e806f
Dominik Donoch <dominik.donoch@houseofangular.io>
2023-11-04 18:15:32 +0100

    feat: add logo

--- libs/blog/layouts/ui/src/lib/layout/layout.component.ts
+++ libs/blog/layouts/ui/src/lib/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'angular-love-layout',
   standalone: true,
-  imports: [RouterModule, FooterComponent],
+  imports: [RouterModule, FooterComponent, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
