0c268043acaad083faabace129cb29563e7257b2
Dominik Donoch <96125413+DDonochVA@users.noreply.github.com>
2023-11-09 00:42:32 +0100

    feat: use application builder (#23)

--- apps/blog/src/app/app.config.ts
+++ apps/blog/src/app/app.config.ts
@@ -4,14 +4,16 @@ import {
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { environment } from '../environments/environment';
 import { provideApollo } from '@angular-love/blog/shared/data-access/graphql';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
     environment.providers,
     provideApollo(),
   ],
