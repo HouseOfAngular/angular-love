5b0d190bf8ac1fd1bfd82cd10b52b84b0424d02f
DamianBrzezinskiHoA <damian.brzezinski@houseofangular.io>
2024-04-26 11:53:33 +0200

    KAP-41/chore-links-appended

--- libs/blog/partners/ui/ui-partners-list/src/lib/partners-list/partners-list.component.ts
+++ libs/blog/partners/ui/ui-partners-list/src/lib/partners-list/partners-list.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 export type PartnerData = {
-  URL: string;
+  asset_URL: string;
   alt: string;
+  link_URL: string;
 };
 
 @Component({
@@ -15,24 +16,34 @@ export type PartnerData = {
 export class PartnersListComponent {
   protected readonly partnersList: PartnerData[] = [
     {
-      URL: 'assets/frontend-united.png',
+      asset_URL: 'assets/frontend-united.png',
       alt: 'frontend united',
+      link_URL: '',
     },
     {
-      URL: 'assets/angular-js.png',
+      asset_URL: 'assets/angular-js.png',
       alt: 'angular-js',
+      link_URL: 'https://ng-poland.pl/',
     },
     {
-      URL: 'assets/angular-community.png',
+      asset_URL: 'assets/angular-community.png',
       alt: 'angular-community',
+      link_URL: 'https://angularcommunity.net/',
     },
     {
-      URL: 'assets/GDG-cloud.png',
+      asset_URL: 'assets/GDG-cloud.png',
       alt: 'GDG-cloud',
+      link_URL: 'https://warsaw.devfest.pl/',
     },
     {
-      URL: 'assets/front-end.png',
+      asset_URL: 'assets/front-end.png',
       alt: 'front-end',
+      link_URL: '',
     },
   ];
+
+  protected visitPage(url: string) {
+    if (url.length < 1) return;
+    window.open(url, '_blank');
+  }
 }
