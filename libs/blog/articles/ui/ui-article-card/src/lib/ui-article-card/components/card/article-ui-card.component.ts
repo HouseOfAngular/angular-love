2769f1ed38f56d4f6c9a1bb302d809ab2f642279
DamianBrzezinskiHoA <damian.brzezinski@houseofangular.io>
2024-04-26 16:26:48 +0200

    KAP-59/chore-refactored-article-ui-card-deleted-removed-directives

--- libs/blog/articles/ui/ui-article-card/src/lib/ui-article-card/components/card/article-ui-card.component.ts
+++ libs/blog/articles/ui/ui-article-card/src/lib/ui-article-card/components/card/article-ui-card.component.ts
@@ -1,17 +1,10 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Directive,
-  ElementRef,
-  Host,
-  input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { AvatarComponent } from '@angular-love/blog/shared/ui/avatar';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { DatePipe, NgOptimizedImage } from '@angular/common';
+import { DatePipe, NgClass, NgOptimizedImage } from '@angular/common';
 import { bootstrapClock } from '@ng-icons/bootstrap-icons';
 
-export type Layout = 'HORIZONTAL' | 'VERTICAL';
+export type Layout = 'regular' | 'horizontal';
 
 export type ArticleData = {
   title: string;
@@ -26,122 +19,11 @@ export type ArticleData = {
   };
 };
 
-@Directive({
-  standalone: true,
-  selector: '[alCardLayoutDirective]',
-})
-export class AlCardLayoutDirective {
-  constructor(
-    private el: ElementRef,
-    @Host() parent: ArticleUiCardComponent,
-  ) {
-    if (parent.layout() === 'VERTICAL') {
-      this.el.nativeElement.classList.add(
-        'flex',
-        'flex-col',
-        'w-[408px]',
-        'h-[450px]',
-      );
-    } else {
-      this.el.nativeElement.classList.add(
-        'flex',
-        'flex-row',
-        'w-[1288px]',
-        'h-[220px]',
-      );
-    }
-  }
-}
-
-@Directive({
-  standalone: true,
-  selector: '[alCardContentLayoutDirective]',
-})
-export class AlCardContentLayoutDirective {
-  constructor(
-    private el: ElementRef,
-    @Host() parent: ArticleUiCardComponent,
-  ) {
-    parent.layout() === 'VERTICAL'
-      ? this.el.nativeElement.classList.add(
-          'w-[408px]',
-          'h-[230px]',
-          'rounded-b-lg',
-        )
-      : this.el.nativeElement.classList.add(
-          'w-[880px]',
-          'h-[220px]',
-          'rounded-r-lg',
-        );
-  }
-}
-
-@Directive({
-  standalone: true,
-  selector: '[alCardImageLayoutDirective]',
-})
-export class AlCardImageLayoutDirective {
-  constructor(
-    private el: ElementRef,
-    @Host() parent: ArticleUiCardComponent,
-  ) {
-    parent.layout() === 'VERTICAL'
-      ? this.el.nativeElement.classList.add(
-          'rounded-t-lg',
-          'background-vertical',
-        )
-      : this.el.nativeElement.classList.add(
-          'rounded-l-lg',
-          'background-horizontal',
-        );
-  }
-}
-
-@Directive({
-  standalone: true,
-  selector: '[alCardHeaderAndExcerptLayoutDirective]',
-})
-export class AlCardHeaderAndExcerptLayoutDirective {
-  constructor(
-    private el: ElementRef,
-    @Host() parent: ArticleUiCardComponent,
-  ) {
-    parent.layout() === 'VERTICAL'
-      ? this.el.nativeElement.classList.add('px-4', 'pt-3', 'pb-4')
-      : this.el.nativeElement.classList.add('px-6', 'pt-8');
-  }
-}
-
-@Directive({
-  standalone: true,
-  selector: '[alCardExcerptLayoutDirective]',
-})
-export class AlCardExcerptLayoutDirective {
-  constructor(
-    private el: ElementRef,
-    @Host() parent: ArticleUiCardComponent,
-  ) {
-    parent.layout() === 'HORIZONTAL'
-      ? this.el.nativeElement.classList.add('pt-5')
-      : '';
-  }
-}
-
 @Component({
   selector: 'al-ui-article-card',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [
-    AvatarComponent,
-    NgIcon,
-    DatePipe,
-    AlCardLayoutDirective,
-    NgOptimizedImage,
-    AlCardContentLayoutDirective,
-    AlCardImageLayoutDirective,
-    AlCardHeaderAndExcerptLayoutDirective,
-    AlCardExcerptLayoutDirective,
-  ],
+  imports: [AvatarComponent, NgIcon, DatePipe, NgOptimizedImage, NgClass],
   templateUrl: './article-ui-card.component.html',
   styleUrl: './article-ui-card.component.scss',
   providers: [
