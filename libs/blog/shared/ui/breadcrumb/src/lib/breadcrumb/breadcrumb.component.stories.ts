fb4e492d082d0b2151e51a2b533821e70576634c
jakNatonek <jakub.natonek@houseofangular.io>
2024-04-29 11:23:16 +0200

    feat(breadcrumb): init lib

---
+++ libs/blog/shared/ui/breadcrumb/src/lib/breadcrumb/breadcrumb.component.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { BreadcrumbComponent } from './breadcrumb.component';
+
+const meta: Meta<BreadcrumbComponent> = {
+  component: BreadcrumbComponent,
+  title: 'Shared UI / BreadCrumb',
+};
+
+export default meta;
+type Story = StoryObj<BreadcrumbComponent>;
+
+export const Primary: Story = {
+  args: {
+    pageName: 'Home',
+  },
+};
