dac92bfd51a61b1ed40a565239ddefedf59ff92b
DamianBrzezinskiHoA <damian.brzezinski@houseofangular.io>
2024-04-29 20:31:59 +0200

    KAP-81/chore-appended-missing-import

--- libs/blog/articles/ui/ui-article-card/src/lib/ui-article-card/ui-article-card.component.ts
+++ libs/blog/articles/ui/ui-article-card/src/lib/ui-article-card/ui-article-card.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { BackgroundArticleCardComponent } from './components/background-card/background-article-card.component';
 import { ArticleCardDataModel } from '@angular-love/article-card-data-model';
+import { ArticleUiClassicCardComponent } from './components/card/article-ui-classic-card.component';
 
 export type CardType = 'regular' | 'horizontal' | 'compact' | 'hero';
 
@@ -10,7 +11,7 @@ export type CardType = 'regular' | 'horizontal' | 'compact' | 'hero';
   templateUrl: './ui-article-card.component.html',
   styleUrl: './ui-article-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [BackgroundArticleCardComponent],
+  imports: [BackgroundArticleCardComponent, ArticleUiClassicCardComponent],
 })
 export class UiArticleCardComponent {
   cardType = input.required<CardType>();
