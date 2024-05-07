53d0fd1adc9f136039f4ce8ae229d774f3de2b99
marcin-hoa <marcin.stelmaszyk@houseofangular.io>
2024-05-07 16:57:47 +0200

    chore(mapper): changed data type

--- libs/blog/search/data-access/src/lib/models/search-dto-to-card-model.mapper.ts
+++ libs/blog/search/data-access/src/lib/models/search-dto-to-card-model.mapper.ts
@@ -13,7 +13,7 @@ export const mapToCardModel = (
     excerpt: dto._highlightResult.content.value.slice(0, 150),
     featuredImageUrl: 'assets/mock-avatar.png', //TODO: Replace with real dto data
     publishDate: dto.post_date_formatted,
-    readingTime: dto.reading_time || 0,
+    readingTime: `${dto.reading_time || 0}`,
     slug: dto.post_slug,
     title: dto._highlightResult.post_title.value,
   };
