2cc60844d0bda1857a4914614fcba371be9d2531
DamianBrzezinskiHoA <damian.brzezinski@houseofangular.io>
2024-05-07 22:00:41 +0200

    chore(icons): appended part of remarks

--- libs/blog/shared/ui/icon/src/lib/icon/icon.component.ts
+++ libs/blog/shared/ui/icon/src/lib/icon/icon.component.ts
@@ -1,32 +1,31 @@
-import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   inject,
   input,
 } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
-import { IconService, IconType } from '../icon-service';
+import { IconType, returnIcon } from '../icons';
 
 export type Size = 16;
 
 @Component({
   selector: 'al-icon',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './icon.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['icon.component.scss'],
 })
 export class IconComponent {
   size = input<Size>(16);
   name = input.required<IconType>();
 
-  private _iconService = inject(IconService);
   private _sanitizer = inject(DomSanitizer);
 
   protected get svgIcon(): SafeHtml {
-    const svgContent = this._iconService.getSvgByName(this.name());
-    return this._sanitizer.bypassSecurityTrustHtml(svgContent);
+    const svgContent = computed(() => returnIcon(this.name()));
+    return this._sanitizer.bypassSecurityTrustHtml(svgContent());
   }
 }
