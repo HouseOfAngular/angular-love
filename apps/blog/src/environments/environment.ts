0eac70500371227c1633c349053ac7fa2a8cbf9f
DamianBrzezinskiHoA <damian.brzezinski@houseofangular.io>
2024-05-08 08:55:51 +0200

    chore(partners-component): remarks appended, dependencies installed

--- apps/blog/src/environments/environment.ts
+++ apps/blog/src/environments/environment.ts
@@ -1,5 +1,3 @@
-import * as process from 'process';
-
 import { provideConfig } from '@angular-love/shared/config';
 
 import { AppEnvironment } from './app-environment';
@@ -8,9 +6,6 @@ export const environment: AppEnvironment = {
   providers: [
     provideConfig({
       apiBaseUrl: process.env.AL_API_URL,
-      alGraphqlUrl: process.env.AL_GRAPHQL_URI,
-      alGraphqlToken: process.env.AL_GRAPHQL_TOKEN,
-      alIsProd: process.env.AL_IS_PROD,
     }),
   ],
 };
--- apps/blog/src/types.d.ts
+++ apps/blog/src/types.d.ts
@@ -1,8 +1,5 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     AL_API_URL: string;
-    AL_GRAPHQL_URI: string;
-    AL_GRAPHQL_TOKEN: string;
-    AL_IS_PROD: boolean;
   }
 }
