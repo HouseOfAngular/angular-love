095438e279a69b69cba5f23722f476c35a018659
Dominik Donoch <dominik.donoch@houseofangular.io>
2024-05-20 21:11:15 +0200

    fix(client): use `routerLink` on article list links

--- libs/blog/articles/ui-article-list-title/src/lib/ui-article-list-title/ui-article-list-title.component.ts
+++ libs/blog/articles/ui-article-list-title/src/lib/ui-article-list-title/ui-article-list-title.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 export type ArticleTitleLink = {
   displayName: string;
@@ -10,6 +11,7 @@ export type ArticleTitleLink = {
   standalone: true,
   templateUrl: './ui-article-list-title.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterLink],
 })
 export class UiArticleListTitleComponent {
   title = input.required<string>();
