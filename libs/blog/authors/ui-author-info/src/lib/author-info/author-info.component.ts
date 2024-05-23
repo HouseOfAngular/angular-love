639ce0dc5983b5cb7369fac4e340de8862f763c7
jakNatonek <jakub.natonek@houseofangular.io>
2024-05-23 15:15:41 +0200

    style(author-info): style adjustments and clampText property

--- libs/blog/authors/ui-author-info/src/lib/author-info/author-info.component.ts
+++ libs/blog/authors/ui-author-info/src/lib/author-info/author-info.component.ts
@@ -1,18 +1,34 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 
 import { UiAuthorInfo } from '@angular-love/blog/authors/types';
 import { AvatarComponent } from '@angular-love/blog/shared/ui-avatar';
+import { DynamicTextClampComponent } from '@angular-love/blog/shared/ui-dynamic-text-clamp';
 
 import { AuthorInfoTemplateComponent } from './author-info-template.component';
 
 @Component({
   selector: 'al-author-info',
   standalone: true,
-  imports: [AuthorInfoTemplateComponent, AvatarComponent],
+  imports: [
+    AuthorInfoTemplateComponent,
+    AvatarComponent,
+    DynamicTextClampComponent,
+  ],
   templateUrl: './author-info.component.html',
   styleUrl: './author-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthorInfoComponent {
   author = input.required<UiAuthorInfo>();
+
+  clampText = input<boolean>();
+
+  descriptionClass = computed(
+    () => 'text-sm' + (this.clampText() ? ' line-clamp-3' : ''),
+  );
 }
