09ef5825e7c6a2975a4e8559a5072485ec8200df
Dominik Donoch <dominik.donoch@houseofangular.io>
2024-05-23 20:03:34 +0200

    chore: bump nx to `19.1.0-beta.2`

--- apps/blog/server.ts
+++ apps/blog/server.ts
@@ -25,14 +25,15 @@ export function app(): express.Express {
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get(
-    '*.*',
+    '**',
     express.static(browserDistFolder, {
       maxAge: '1y',
+      index: 'index.html',
     }),
   );
 
   // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
+  server.get('**', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
     commonEngine
