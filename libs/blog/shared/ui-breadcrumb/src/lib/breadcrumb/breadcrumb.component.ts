c222c60720b7b94b07b76ac0b209dff97fc6571b
Dominik Donoch <dominik.donoch@houseofangular.io>
2024-06-12 21:50:55 +0200

    fix(client): breadcrumb links

--- libs/blog/shared/ui-breadcrumb/src/lib/breadcrumb/breadcrumb.component.ts
+++ libs/blog/shared/ui-breadcrumb/src/lib/breadcrumb/breadcrumb.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslocoDirective } from '@ngneat/transloco';
+import { LocalizeRouterModule } from '@penleychan/ngx-transloco-router';
 
 @Component({
   selector: 'al-breadcrumb',
   standalone: true,
-  imports: [TranslocoDirective],
+  imports: [TranslocoDirective, RouterLink, LocalizeRouterModule],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
