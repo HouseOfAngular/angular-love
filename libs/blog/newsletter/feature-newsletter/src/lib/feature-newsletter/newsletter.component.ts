889b7c35c12ca547bfeb89183f620cea00918bf8
DamianBrzezinskiHoA <damian.brzezinski@houseofangular.io>
2024-06-19 16:23:16 +0200

    feat(newsletter-card): error state handling

--- libs/blog/newsletter/feature-newsletter/src/lib/feature-newsletter/newsletter.component.ts
+++ libs/blog/newsletter/feature-newsletter/src/lib/feature-newsletter/newsletter.component.ts
@@ -32,7 +32,6 @@ import { NewsletterStore } from '@angular-love/data-access';
     TranslocoDirective,
   ],
   templateUrl: './newsletter.component.html',
-  styleUrl: './newsletter.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [NewsletterStore],
 })
@@ -50,17 +49,18 @@ export class NewsletterComponent {
   private readonly _router = inject(Router);
   private readonly _localizeRouter = inject(LocalizeRouterService);
   private readonly _onSuccess = effect(() => {
-    if (this._newsletterStore.loading() === 'success') {
-      this._router.navigate(
-        this._localizeRouter.translateRoute(['/newsletter']) as string[],
-        {
-          queryParams: {
-            nm: 'confirmed',
-          },
+    if (this.newsletterStoreLoading() !== 'success') return;
+    this._router.navigate(
+      this._localizeRouter.translateRoute(['/newsletter']) as string[],
+      {
+        queryParams: {
+          nm: 'confirmed',
         },
-      );
-    }
+      },
+    );
   });
+
+  protected readonly newsletterStoreLoading = this._newsletterStore.loading;
 
   postEmailAddress(): void {
     // TODO: find good approach to handle with invalid form - alert in not acceptable - in parallel with error state handling
