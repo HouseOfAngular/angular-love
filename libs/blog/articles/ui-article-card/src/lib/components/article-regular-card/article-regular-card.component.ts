b400285b3978ee19543692529e75cde0b93727eb
Dominik Donoch <dominik.donoch@houseofangular.io>
2024-07-08 23:27:24 +0200

    perf: remove sanitizer from regular card

--- libs/blog/articles/ui-article-card/src/lib/components/article-regular-card/article-regular-card.component.ts
+++ libs/blog/articles/ui-article-card/src/lib/components/article-regular-card/article-regular-card.component.ts
@@ -9,19 +9,12 @@ import {
 } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { RouterLink } from '@angular/router';
+import { FastSvgComponent } from '@push-based/ngx-fast-svg';
 
 import { AlLocalizePipe } from '@angular-love/blog/i18n/util';
 import { ArticleCard } from '@angular-love/blog/shared/types';
 import { AvatarComponent } from '@angular-love/blog/shared/ui-avatar';
 import { UiDifficultyComponent } from '@angular-love/blog/shared/ui-difficulty';
-import { FastSvgComponent } from "@push-based/ngx-fast-svg";
-
-export type Layout = 'regular' | 'horizontal';
-
-type SanitizedArticleDataModel = {
-  title: string;
-  excerpt: string;
-};
 
 @Component({
   selector: 'al-article-regular-card',
@@ -42,17 +35,4 @@ type SanitizedArticleDataModel = {
 export class ArticleRegularCardComponent {
   article = input.required<ArticleCard>();
   imagePriority = input<number | null>(null);
-
-  sanitizedArticle = computed<SanitizedArticleDataModel>(() => {
-    return {
-      excerpt: this.sanitize(this.article().excerpt),
-      title: this.sanitize(this.article().title),
-    };
-  });
-
-  private readonly domSanitizer = inject(DomSanitizer);
-
-  private sanitize(val: string): string {
-    return this.domSanitizer.sanitize(SecurityContext.HTML, val) || '';
-  }
 }
