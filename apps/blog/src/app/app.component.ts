efe59d67fe271aef982fb9d37573ceadd9ba2297
Artur Androsovych <arthurandrosovich@gmail.com>
2024-07-09 14:56:39 +0100

    refactor: self-host fonts (#225)

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -13,5 +13,27 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   constructor(viewport: ViewportScroller) {
     viewport.setOffset([0, 80]);
+
+    // Our fonts are self-hosted.
+    // https://fonts.google.com/knowledge/using_type/self_hosting_web_fonts
+    // We could also preload the fonts that are primarily used, such as
+    // Inter Latin and Inter Latin-ext. However, we have multiple unicode-ranges
+    // defined, which complicates things. There's no way to define the unicode-range
+    // in the `link` media property, for example. Therefore, the font would only be
+    // preloaded if the user's browser supports that unicode-range. Nevertheless, we
+    // can choose to always preload the Latin subset of the Inter font.
   }
+
+  // Leaving this as commented until the decision is made to always preload the Inter font.
+  // ngAfterViewInit(): void {
+  //   if (this._isBrowser) return;
+
+  //   const interLatinLink = this._document.createElement('link');
+  //   interLatinLink.rel = 'preload';
+  //   interLatinLink.href = '/assets/fonts/inter-v13-latin.woff2';
+  //   interLatinLink.as = 'font';
+  //   interLatinLink.type = 'font/woff2';
+
+  //   this._document.head.appendChild(interLatinLink);
+  // }
 }
