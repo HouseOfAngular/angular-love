a327a707b67e1fe01d84f573eea74ddf083f98b2
Dominik Donoch <dominik.donoch@houseofangular.io>
2024-07-09 23:48:22 +0200

    fix: switch languages in guards instead of resolvers

--- libs/blog/i18n/data-access/src/lib/guard/active-language.guard.ts
+++ libs/blog/i18n/data-access/src/lib/guard/active-language.guard.ts
@@ -1,14 +1,13 @@
 import { inject } from '@angular/core';
-import { ResolveFn } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { TranslocoService } from '@jsverse/transloco';
 
-export const resolveActiveLanguage: (lang: string) => ResolveFn<void> = (
-  lang,
-) => {
+export const activeLanguageGuard: (lang: string) => CanActivateFn = (lang) => {
   return () => {
     const translocoService = inject(TranslocoService);
     if (translocoService.getActiveLang() !== lang) {
       translocoService.setActiveLang(lang);
     }
+    return true;
   };
 };
