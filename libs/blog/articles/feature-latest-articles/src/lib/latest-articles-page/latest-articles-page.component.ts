1543b8428927c535d0d2791a3754c5ba374fac90
mateuszbasinski <mateusz.basinski@valueadd.pl>
2024-09-09 12:01:57 +0200

    fix(blog): wrong category slug

--- libs/blog/articles/feature-latest-articles/src/lib/latest-articles-page/latest-articles-page.component.ts
+++ libs/blog/articles/feature-latest-articles/src/lib/latest-articles-page/latest-articles-page.component.ts
@@ -43,7 +43,7 @@ export class FeatureLatestArticlesPageComponent {
   constructor() {
     const query = computed(() => ({
       ...this.pagination(),
-      excludeCategory: 'angular-in-depth-en',
+      excludeCategory: 'angular-in-depth',
     }));
 
     this.articleStore.fetchArticleList(query);
