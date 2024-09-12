a64571d251854f7f23f9f99d94fb5437ca17ac70
DominikKalinowski <dominikkalinowski10@gmail.com>
2024-09-12 15:53:30 +0200

    feat(client): customize some details in light mode
    Implements KAP-237 KAP-235

--- tailwind.preset.js
+++ tailwind.preset.js
@@ -1,3 +1,9 @@
+const {
+  themeVariants,
+  prefersLight,
+  prefersDark,
+} = require('tailwindcss-theme-variants');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
@@ -29,5 +35,17 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/container-queries')],
+  plugins: [
+    require('@tailwindcss/container-queries'),
+    themeVariants({
+      themes: {
+        light: {
+          mediaQuery: prefersLight /* "@media (prefers-color-scheme: light)" */,
+        },
+        dark: {
+          mediaQuery: prefersDark /* "@media (prefers-color-scheme: dark)" */,
+        },
+      },
+    }),
+  ],
 };
