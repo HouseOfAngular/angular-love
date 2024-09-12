fbb915a6aa7d21adafc0b3752a5b53d3249d098e
majahendzel-va <110097906+majahendzel-va@users.noreply.github.com>
2024-09-12 09:38:09 +0200

    fix(blog): storybook errors (#309)

--- libs/blog/articles/ui-article-card/src/lib/ui-article-card/ui-article-card.stories.ts
+++ libs/blog/articles/ui-article-card/src/lib/ui-article-card/ui-article-card.stories.ts
@@ -1,10 +1,21 @@
-import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { provideTransloco, translocoConfig } from '@jsverse/transloco';
+import {
+  applicationConfig,
+  Meta,
+  moduleMetadata,
+  StoryObj,
+} from '@storybook/angular';
 
+import { TranslocoHttpLoader } from '@angular-love/blog/i18n/data-access';
+import { AlLocalizeService } from '@angular-love/blog/i18n/util';
 import { ArticleCard } from '@angular-love/blog/shared/types';
 
+import { ArticleCompactCardComponent } from '../components/article-compact-card/article-compact-card.component';
+import { ArticleHeroCardComponent } from '../components/article-hero-card/article-hero-card.component';
+import { ArticleHorizontalCardComponent } from '../components/article-horizontal-card/article-horizontal-card.component';
 import { ArticleRegularCardComponent } from '../components/article-regular-card/article-regular-card.component';
 
-import { CardType, UiArticleCardComponent } from './ui-article-card.component';
+import { UiArticleCardComponent } from './ui-article-card.component';
 
 const article: ArticleCard = {
   title: 'Angular Storybook',
@@ -21,20 +32,38 @@ const article: ArticleCard = {
   publishDate: new Date('2021-05-01').toISOString(),
 };
 
-const layoutCompact: CardType = 'compact';
-const layoutHero: CardType = 'hero';
-const layoutRegular: CardType = 'regular';
-const layoutHorizontal: CardType = 'horizontal';
+const translocoConf = translocoConfig({
+  availableLangs: ['pl', 'en'],
+  defaultLang: 'pl',
+});
 
 const meta: Meta<UiArticleCardComponent> = {
   component: UiArticleCardComponent,
   subcomponents: {
     ArticleUiCardComponent: ArticleRegularCardComponent,
+    ArticleCompactCardComponent: ArticleCompactCardComponent,
+    ArticleHeroCardComponent: ArticleHeroCardComponent,
+    ArticleHorizontalCardComponent: ArticleHorizontalCardComponent,
   },
   title: 'Articles / UiArticleCardComponent',
   decorators: [
     moduleMetadata({
-      imports: [ArticleRegularCardComponent],
+      imports: [
+        ArticleRegularCardComponent,
+        ArticleCompactCardComponent,
+        ArticleHeroCardComponent,
+        ArticleHorizontalCardComponent,
+      ],
+      providers: [AlLocalizeService],
+    }),
+
+    applicationConfig({
+      providers: [
+        provideTransloco({
+          config: translocoConf,
+          loader: TranslocoHttpLoader,
+        }),
+      ],
     }),
   ],
 };
@@ -45,15 +74,12 @@ type Story = StoryObj<UiArticleCardComponent>;
 export const compact: Story = {
   args: {
     article: article,
-    cardType: layoutCompact,
   },
   render: (args) => ({
     props: args,
     template: `
-      <div class="w-[408px] h-[232px]">
-        <al-article-regular-card [article]="article" [cardType]="cardType">
-          <al-article-hero-card [article]="article" [layout]="cardType.layout"></al-article-hero-card>
-        </al-article-regular-card>
+      <div style="width: 405px;">
+        <al-article-compact-card [article]="article"></al-article-compact-card>
       </div>
     `,
   }),
@@ -62,15 +88,12 @@ export const compact: Story = {
 export const hero: Story = {
   args: {
     article: article,
-    cardType: layoutHero,
   },
   render: (args) => ({
     props: args,
     template: `
-     <div class="h-[430px]">
-      <al-article-regular-card [article]="article" [cardType]="cardType">
-        <al-article-hero-card [article]="article" [layout]="cardType.layout"></al-article-hero-card>
-      </al-article-regular-card>
+     <div style="width: 816px;">
+       <al-article-hero-card [article]="article"></al-article-hero-card>
      </div>`,
   }),
 };
@@ -78,15 +101,12 @@ export const hero: Story = {
 export const horizontal: Story = {
   args: {
     article: article,
-    cardType: layoutHorizontal,
   },
   render: (args) => ({
     props: args,
     template: `
-     <div style="width: 100%; height: 230px;">
-      <al-article-regular-card [article]="article" [cardType]="cardType">
-        <al-article-regular-card [article]="article" [layout]="cardType.layout"></al-article-regular-card>
-      </al-article-regular-card>
+     <div style="height: 230px;">
+        <al-article-horizontal-card [article]="article"></al-article-horizontal-card>
      </div>`,
   }),
 };
@@ -94,15 +114,12 @@ export const horizontal: Story = {
 export const regular: Story = {
   args: {
     article: article,
-    cardType: layoutRegular,
   },
   render: (args) => ({
     props: args,
     template: `
-     <div style="width: 460px; height: 600px;">
-      <al-article-regular-card [article]="article" [cardType]="cardType">
-        <al-article-regular-card [article]="article" [layout]="cardType.layout"></al-article-regular-card>
-      </al-article-regular-card>
+     <div style="width: 405px; height: 514px;">
+        <al-article-regular-card [article]="article"></al-article-regular-card>
      </div>`,
   }),
 };
--- libs/blog/shared/ui-pagination/src/lib/pagination/pagination.component.stories.ts
+++ libs/blog/shared/ui-pagination/src/lib/pagination/pagination.component.stories.ts
@@ -1,10 +1,28 @@
-import { Meta, StoryObj } from '@storybook/angular';
+import { provideTransloco, translocoConfig } from '@jsverse/transloco';
+import { applicationConfig, Meta, StoryObj } from '@storybook/angular';
+
+import { TranslocoHttpLoader } from '@angular-love/blog/i18n/data-access';
 
 import { PaginationComponent } from './pagination.component';
+
+const translocoConf = translocoConfig({
+  availableLangs: ['pl', 'en'],
+  defaultLang: 'pl',
+});
 
 const meta: Meta<PaginationComponent> = {
   component: PaginationComponent,
   title: 'Shared UI / Pagination',
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideTransloco({
+          config: translocoConf,
+          loader: TranslocoHttpLoader,
+        }),
+      ],
+    }),
+  ],
 };
 
 export default meta;
