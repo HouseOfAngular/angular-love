886abff832ab4cf44b4c4d4b70e48972ec0fc937
Dominik Donoch <96125413+DDonochVA@users.noreply.github.com>
2024-11-05 15:48:28 +0100

    feat: global ad banner (#346)

--- libs/blog/home/feature-home/src/lib/home-page/home-page.component.ts
+++ libs/blog/home/feature-home/src/lib/home-page/home-page.component.ts
@@ -1,21 +1,10 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AdBannerStore } from '@angular-love/blog/ad-banner/data-access';
 import { ArticlesListContainerComponent } from '@angular-love/blog/articles/feature-list';
 import { UiArticleCardComponent } from '@angular-love/blog/articles/ui-article-card';
 import { NewsletterComponent } from '@angular-love/blog/newsletter';
 import { PartnersComponent } from '@angular-love/blog/partners/ui-partners';
-import {
-  AdImageBanner,
-  AdImageBannerComponent,
-  AlBannerCarouselComponent,
-} from '@angular-love/blog/shared/ad-banner';
 import { CardComponent } from '@angular-love/blog/shared/ui-card';
 import { FeatureLatestArticlesComponent } from '@angular-love/feature-latest-articles';
 
@@ -33,8 +22,6 @@ import { hoaHireUs, partnersList } from './partners';
     FeatureLatestArticlesComponent,
     UiArticleCardComponent,
     ArticlesListContainerComponent,
-    AdImageBannerComponent,
-    AlBannerCarouselComponent,
   ],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
@@ -43,22 +30,4 @@ import { hoaHireUs, partnersList } from './partners';
 export class HomePageComponent {
   protected readonly hoaHireUs = hoaHireUs;
   protected readonly partnersList = partnersList;
-  protected readonly sliderStore = inject(AdBannerStore);
-  protected readonly slides = computed<AdImageBanner[] | undefined>(() =>
-    this.sliderStore.slider()?.slides.map((slide) => ({
-      url: slide.url,
-      alt: slide.alt,
-      action: {
-        type: 'url',
-        url: slide.navigateTo,
-      },
-    })),
-  );
-  protected readonly msPerSlide = computed(
-    () => this.sliderStore.slider()?.slideDisplayTimeMs,
-  );
-
-  constructor() {
-    this.sliderStore.getData();
-  }
 }
