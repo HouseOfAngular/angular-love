c9ddc50638821ce56dc64cc7533995dee5a7c3ff
Dominik Donoch <dominik.donoch@houseofangular.io>
2025-02-21 21:14:54 +0100

    refactor(blog): remove AID banner

--- libs/blog/ad-banner/ui/src/index.ts
+++ libs/blog/ad-banner/ui/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/ad-image-banner/ad-image-banner.component';
 export * from './lib/ad-image-banner/ad-image-banner-data.interface';
-export * from './lib/instances/al-indepth-banner.component';
 export * from './lib/banner-carousel/al-banner-carousel.component';
