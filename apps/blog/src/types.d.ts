e1b4f29073abaf1be8e45c3a2b19c135e6ebf351
Dominik Donoch <dominik.donoch@houseofangular.io>
2025-03-11 23:17:01 +0100

    refactor: adjust client to new bff with turso

--- apps/blog/src/types.d.ts
+++ apps/blog/src/types.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     AL_BASE_URL: string;
     AL_API_URL: string;
-    AL_API_URL_V2: string;
     AL_ALGOLIA_APPLICATION_ID: string;
     AL_ALGOLIA_INDEX_NAME: string;
     AL_ALGOLIA_API_KEY: string;
