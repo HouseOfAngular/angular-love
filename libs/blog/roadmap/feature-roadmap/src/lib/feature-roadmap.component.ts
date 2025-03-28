4dd6c37985ce31017ac5dea6b3dfba4a915711a1
DominikKalinowski <dominikkalinowski10@gmail.com>
2025-03-28 17:37:11 +0100

    feat: use vflow instead of swimlane, fill the whole workspace with roadmap svg

--- libs/blog/roadmap/feature-roadmap/src/lib/feature-roadmap.component.ts
+++ libs/blog/roadmap/feature-roadmap/src/lib/feature-roadmap.component.ts
@@ -1,25 +1,50 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NgxGraphModule, Orientation } from '@swimlane/ngx-graph';
+import { Edge, Node, Vflow } from 'ngx-vflow';
 
-// class CustomLayout implements Layout {
-//   updateEdge(graph: Graph, edge: Edge): Graph | Observable<Graph> {}
-//
-//   run(graph: any): any {
-//     return graph;
-//   }
-// }
+import { UiRoadmapNodeComponent } from './ui-roadmap-node.component';
 
 @Component({
   selector: 'al-feature-roadmap',
-  imports: [NgxGraphModule],
+  imports: [Vflow, UiRoadmapNodeComponent],
   templateUrl: './feature-roadmap.component.html',
   styleUrl: './feature-roadmap.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeatureRoadmapComponent {
-  onNodeClick($event: any) {
-    console.log('Node clicked', $event);
-  }
+  nodes: Node[] = [
+    {
+      id: '1',
+      point: { x: 10, y: 200 },
+      type: 'html-template',
+      data: {
+        title: 'Node 1',
+        description: 'This is node 1',
+      },
+    },
+    {
+      id: '2',
+      point: { x: 200, y: 100 },
+      type: 'default',
+      text: '2',
+    },
+    {
+      id: '3',
+      point: { x: 200, y: 300 },
+      type: 'default',
+      text: '3',
+    },
+  ];
 
-  protected readonly Orientation = Orientation;
+  edges: Edge[] = [
+    {
+      id: '1 -> 2',
+      source: '1',
+      target: '2',
+    },
+    {
+      id: '1 -> 3',
+      source: '1',
+      target: '3',
+    },
+  ];
 }
