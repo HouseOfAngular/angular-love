4e0b91133051c65fadd2aa439bb631cd5e5f9cf1
Dominik Donoch <dominik.donoch@houseofangular.io>
2025-04-01 17:23:31 +0200

    refactor(bff): remove cache from banners api

--- libs/blog-bff/banners/api/src/lib/api.ts
+++ libs/blog-bff/banners/api/src/lib/api.ts
@@ -1,15 +1,12 @@
 import { Hono } from 'hono';
 
-import {
-  appCache,
-  langMw,
-} from '@angular-love/blog-bff/shared/util-middleware';
+import { langMw } from '@angular-love/blog-bff/shared/util-middleware';
 import { wpClientMw } from '@angular-love/util-wp';
 
 import { toBanner } from './mappers';
 import { WpBanners } from './wp-banners';
 
-const app = new Hono().use(appCache).use(wpClientMw).use(langMw());
+const app = new Hono().use(wpClientMw).use(langMw());
 
 app.get('/', async (c) => {
   const client = new WpBanners(c.var.createWPClient());
