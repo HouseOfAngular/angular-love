5ff4b98de1ff5eebe3b62c0cdbb7858078b40cf2
DominikKalinowski <dominikkalinowski10@gmail.com>
2025-04-15 08:12:46 +0200

    feat(client): use new top banner (spring camp)

--- libs/blog/ad-banner/ui/src/lib/top-banner.component.ts
+++ libs/blog/ad-banner/ui/src/lib/top-banner.component.ts
@@ -6,13 +6,12 @@ import { Component } from '@angular/core';
     <a
       class="flex w-full flex-wrap justify-center gap-[5px] px-[10px] py-3 text-[18px] font-medium text-white"
       style="background-image: linear-gradient(0.25turn, #fe5758, #8d52fe)"
-      href="https://houseofangular.io/the-ultimate-guide-to-angular-evolution/?utm_source=www-al&utm_medium=baner&utm_campaign=angular19-evolution"
+      href="https://www.youtube.com/watch?v=Ygnx8eH4acg"
     >
-      <span>Enhance Your Project with</span>
       <span class="rounded-[4px] bg-white px-[6px] text-[#b10620]">
-        Angular 19
+        ANGULAR SPRING CAMP
       </span>
-      <span class="underline">Download a free ebook!</span>
+      <span>&nbsp;- JOIN LIVE TODAY AT 6 PM (CEST)</span>
     </a>
   `,
 })
