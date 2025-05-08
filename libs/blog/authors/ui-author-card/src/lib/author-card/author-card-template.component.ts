97ac58bcbf7333629b234167cc845f481cbf6c25
Marcin Stelmaszyk <marcin.stelmaszyk@houseofangular.io>
2025-05-08 17:09:52 +0200

    refactor: replaced div with al-card

--- libs/blog/authors/ui-author-card/src/lib/author-card/author-card-template.component.ts
+++ libs/blog/authors/ui-author-card/src/lib/author-card/author-card-template.component.ts
@@ -25,15 +25,17 @@ import {
         <div
           class="@3xl:flex-row @3xl:border-none flex w-full flex-col items-center rounded-lg border"
         >
-          <div
-            class="@3xl:border dark:@3xl:!bg-al-radial-gradient dark:@3xl:bg-al-background light:@3xl:bg-[#f2f2f2] @3xl:min-w-[260px] min-w-fit rounded-lg pb-4 pt-6"
+          <al-card
+            alGradientCard
+            class="@3xl:border @3xl:min-w-[260px] min-w-fit rounded-lg pb-4 pt-6"
           >
             <div
+              alCardContent
               class="@3xl:max-w-[360px] flex w-full flex-col items-center gap-4"
             >
               <ng-content select="[author-info-card]"></ng-content>
             </div>
-          </div>
+          </al-card>
 
           <div
             class="@3xl:pt-6 w-full flex-1 hyphens-auto break-words p-6 pt-0"
