1851ce35a2c986d6bba4d136fbd694e3cb8b878e
Marcin Stelmaszyk <marcin.stelmaszyk@houseofangular.io>
2025-05-15 15:45:49 +0200

    feat(newsletter): replaced content of newsletter-page and added name field in the form

--- libs/blog/newsletter/feature-newsletter/src/lib/page/newsletter-page.component.ts
+++ libs/blog/newsletter/feature-newsletter/src/lib/page/newsletter-page.component.ts
@@ -1,43 +1,15 @@
 import { A11yModule } from '@angular/cdk/a11y';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { TranslocoDirective } from '@jsverse/transloco';
-import { FastSvgComponent } from '@push-based/ngx-fast-svg';
 
-import { AlLocalizePipe } from '@angular-love/blog/i18n/util';
-import { ButtonComponent } from '@angular-love/blog/shared/ui-button';
+import { CardComponent } from '@angular-love/blog/shared/ui-card';
+
+import { NewsletterComponent } from '../feature-newsletter/newsletter.component';
 
 @Component({
   selector: 'al-newsletter-page',
-  imports: [
-    RouterLink,
-    ButtonComponent,
-    TranslocoDirective,
-    A11yModule,
-    AlLocalizePipe,
-    FastSvgComponent,
-  ],
-  template: `
-    <div
-      *transloco="let t; read: 'newsletterPage'"
-      class="flex flex-col items-center justify-center py-5 text-center"
-      [cdkTrapFocusAutoCapture]="true"
-      cdkTrapFocus
-    >
-      <fast-svg name="circle-check" size="80" />
-      <h2 class="my-2 text-2xl font-bold">
-        {{ t('title') }}
-      </h2>
-      <p class="my-3">
-        {{ t('description') }}
-      </p>
-      <a [routerLink]="['/'] | alLocalize">
-        <button al-button type="button" cdkFocusInitial>
-          {{ t('button') }}
-        </button>
-      </a>
-    </div>
-  `,
+  imports: [TranslocoDirective, A11yModule, CardComponent, NewsletterComponent],
+  templateUrl: './newsletter-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NewsletterPageComponent {}
