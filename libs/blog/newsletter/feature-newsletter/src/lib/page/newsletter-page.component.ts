fc5e887a33f54417ff1ad69b756e657ce9b77af3
Marcin Stelmaszyk <marcin.stelmaszyk@houseofangular.io>
2025-05-29 14:30:51 +0200

    feat: added success case to newsletter-page component

--- libs/blog/newsletter/feature-newsletter/src/lib/page/newsletter-page.component.ts
+++ libs/blog/newsletter/feature-newsletter/src/lib/page/newsletter-page.component.ts
@@ -1,15 +1,37 @@
 import { A11yModule } from '@angular/cdk/a11y';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
 import { TranslocoDirective } from '@jsverse/transloco';
 
 import { CardComponent } from '@angular-love/blog/shared/ui-card';
 
 import { NewsletterComponent } from '../feature-newsletter/newsletter.component';
 
+import { NewsletterSuccesComponent } from './newsletter-succes/newsletter-succes.component';
+
 @Component({
   selector: 'al-newsletter-page',
-  imports: [TranslocoDirective, A11yModule, CardComponent, NewsletterComponent],
+  imports: [
+    TranslocoDirective,
+    A11yModule,
+    CardComponent,
+    NewsletterComponent,
+    NewsletterSuccesComponent,
+  ],
   templateUrl: './newsletter-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NewsletterPageComponent {}
+export class NewsletterPageComponent {
+  private route = inject(ActivatedRoute);
+  private params = toSignal(this.route.queryParams);
+
+  protected readonly isConfirmationPage = computed(
+    () => this.params()?.['nm'] === 'confirmed',
+  );
+}
