99c2f3906daa933c9a0a1da7e15f81a2c363d0db
Daniel Lipniacki <daniel.lipniacki@houseofangular.io>
2025-10-02 14:11:09 +0200

    feat: remove newsletter subscribe on newsletter sub

--- libs/blog-bff/newsletter/src/lib/newsletter-client.ts
+++ libs/blog-bff/newsletter/src/lib/newsletter-client.ts
@@ -6,17 +6,6 @@ export class NewsletterClient {
     private _apiKey: string,
   ) {}
 
-  getTemplate(templateId: number): Promise<Template> {
-    return this.request<Template>(`smtp/templates/${templateId}`);
-  }
-
-  sendEmail(dto: SendEmailDto): Promise<void> {
-    return this.request<void>(`smtp/email`, {
-      method: 'POST',
-      body: dto,
-    });
-  }
-
   createContact(contact: NewContactDto): Promise<void> {
     return this.request(`contacts`, {
       method: 'POST',
