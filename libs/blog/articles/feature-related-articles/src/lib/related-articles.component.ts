123bf52a2190dac8e0e2fe384587706ba8d102dc
Damian Maduzia <damian.maduzia@houseofangular.io>
2025-10-24 13:19:46 +0200

    feat: featured articles carousel poc

--- libs/blog/articles/feature-related-articles/src/lib/related-articles.component.ts
+++ libs/blog/articles/feature-related-articles/src/lib/related-articles.component.ts
@@ -5,6 +5,7 @@ import {
   input,
   OnInit,
 } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 import { RelatedArticleListStore } from '@angular-love/blog/articles/data-access';
 import { UiArticleCardComponent } from '@angular-love/blog/articles/ui-article-card';
@@ -14,19 +15,44 @@ import { UiArticleCardComponent } from '@angular-love/blog/articles/ui-article-c
   template: `
     @if (store.isFetchRelatedArticleListLoaded()) {
       <strong class="text-3xl">Related Articles</strong>
-      <div class="mt-4 grid grid-cols-1 gap-6 p-2 md:grid-cols-2">
+      <owl-carousel-o [options]="customOptions">
         @for (article of store.relatedArticles(); track $index) {
-          <al-article-card [article]="article" cardType="compact" />
+          <ng-template carouselSlide>
+            <al-article-card [article]="article" cardType="compact" />
+          </ng-template>
+          <ng-template carouselSlide>
+            <al-article-card [article]="article" cardType="compact" />
+          </ng-template>
         }
-      </div>
+      </owl-carousel-o>
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [RelatedArticleListStore],
-  imports: [UiArticleCardComponent],
+  imports: [UiArticleCardComponent, CarouselModule],
 })
 export class RelatedArticlesComponent implements OnInit {
   readonly id = input.required<number>();
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: true,
+    pullDrag: false,
+    dots: true,
+    margin: 24, // Add spacing between items
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      // Keep in mind these breakpoints refer to container width, not the viewport width
+      0: {
+        items: 1,
+      },
+      640: {
+        items: 2,
+      },
+    },
+    nav: true,
+  };
 
   readonly store = inject(RelatedArticleListStore);
 
