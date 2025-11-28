7ff7a948ec6a45bf596d03844b758521cbb84a1a
Dominik Donoch <96125413+DDonochVA@users.noreply.github.com>
2025-11-28 05:16:08 -0500

    perf: reduce layout shifts (#491)

--- libs/blog/home/feature-home/src/lib/home-page/welcome-message/welcome-message.component.ts
+++ libs/blog/home/feature-home/src/lib/home-page/welcome-message/welcome-message.component.ts
@@ -6,24 +6,25 @@ import { TranslocoDirective } from '@jsverse/transloco';
   selector: 'al-welcome-message',
   template: `
     <section
-      *transloco="let t; read: 'homePage.welcomeMessage'"
-      class="flex flex-col items-center justify-center space-y-3 py-3 text-center"
+      class="flex flex-col items-center justify-center gap-3 py-3 text-center"
     >
-      <h2 class="text-4xl font-bold">Angular.love Blog</h2>
-      <p class="px-8">
-        {{ t('description') }}
-      </p>
-      <p class="px-8">
-        Powered by
-        <a
-          class="text-al-primary font-bold hover:underline"
-          [attr.aria-label]="t('descriptionAriaLabel')"
-          target="_blank"
-          href="https://houseofangular.io/"
-        >
-          House of Angular
-        </a>
-      </p>
+      <ng-container *transloco="let t; read: 'homePage.welcomeMessage'">
+        <h2 class="text-4xl font-bold">Angular.love Blog</h2>
+        <p class="px-8">
+          {{ t('description') }}
+        </p>
+        <p class="px-8">
+          Powered by
+          <a
+            class="text-al-primary font-bold hover:underline"
+            [attr.aria-label]="t('descriptionAriaLabel')"
+            target="_blank"
+            href="https://houseofangular.io/"
+          >
+            House of Angular
+          </a>
+        </p>
+      </ng-container>
     </section>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
