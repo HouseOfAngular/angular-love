a81358908b1d4be701c0333695d24a111cfe9216
Dominik Donoch <dominik.donoch@houseofangular.io>
2026-03-10 19:05:16 +0100

    feat: generate base analog app

---
+++ apps/blog-analog/vite.config.ts
@@ -0,0 +1,31 @@
+/// <reference types="vitest" />
+
+import analog from '@analogjs/platform';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    root: __dirname,
+    cacheDir: `../../node_modules/.vite`,
+    build: {
+      outDir: '../../dist/apps/blog-analog/client',
+      reportCompressedSize: true,
+      target: ['es2020'],
+    },
+    server: {
+      fs: {
+        allow: ['.'],
+      },
+    },
+    plugins: [analog(), nxViteTsPaths()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      reporters: ['default'],
+    },
+  };
+});
