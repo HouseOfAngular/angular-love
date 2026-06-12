dd649e46b9bcc244320f428875ba6183f34df2ef
majahendzel-va <110097906+majahendzel-va@users.noreply.github.com>
2026-06-12 13:07:47 +0200

    feat(main): add banner in writing-rules (#542)
    Co-authored-by: maja <maja.hendzel@houseofangular.io>

--- libs/blog/become-author/feature-become-author-page/src/index.ts
+++ libs/blog/become-author/feature-become-author-page/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/become-author-page/become-author-page.component';
+export * from './lib/components/become-author-advertisement/become-author-advertisement.component';
--- libs/blog/writing-rules/feature-writing-rules/src/lib/writing-rules/writing-rules.component.ts
+++ libs/blog/writing-rules/feature-writing-rules/src/lib/writing-rules/writing-rules.component.ts
@@ -7,13 +7,27 @@ import {
 } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 
+import { BecomeAuthorAdvertisementComponent } from '@angular-love/blog/become-author-page-feature';
+import { ButtonComponent } from '@angular-love/blog/shared/ui-button';
+import {
+  CardComponent,
+  GradientCardDirective,
+} from '@angular-love/blog/shared/ui-card';
+
 import { RulesRowComponent } from '../rules-row/rules-row.component';
 
 import { rulesContentData } from './rules-content';
 
 @Component({
   selector: 'al-writing-rules',
-  imports: [TranslocoDirective, RulesRowComponent],
+  imports: [
+    TranslocoDirective,
+    RulesRowComponent,
+    BecomeAuthorAdvertisementComponent,
+    ButtonComponent,
+    CardComponent,
+    GradientCardDirective,
+  ],
   templateUrl: './writing-rules.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
