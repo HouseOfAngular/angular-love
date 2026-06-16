5f3bce2ea76dc1edab57eb204dbd896044a3e118
Dawid Gruszczyński <dawid.gruszczynski@houseofangular.io>
2026-06-16 16:09:44 +0200

    feat(angular-talks): change video card styling and behavior to mirror article card

--- libs/blog/videos/ui-video-card/src/lib/components/video-card/video-card-skeleton.component.ts
+++ libs/blog/videos/ui-video-card/src/lib/components/video-card/video-card-skeleton.component.ts
@@ -5,10 +5,16 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
   selector: 'al-video-card-skeleton',
   imports: [NgxSkeletonLoaderModule],
   template: `
-    <div class="flex flex-col">
-      <ngx-skeleton-loader [theme]="{ width: '100%', height: '228px' }" />
+    <div
+      class="bg-al-card h-full w-full rounded-lg border shadow-none dark:border-transparent"
+    >
+      <div class="overflow-hidden rounded-t-lg">
+        <ngx-skeleton-loader
+          [theme]="{ width: '100%', margin: '0', height: '228px' }"
+        />
+      </div>
 
-      <div class="flex flex-col pt-4">
+      <div class="flex flex-col px-4 pt-4 pb-4">
         <!-- video title -->
         <ngx-skeleton-loader [theme]="{ width: '100%', height: '24px' }" />
         <!-- event name -->
