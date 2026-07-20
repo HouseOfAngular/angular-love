2e02a3ba0e0be4ee90f0f4510e20ea64ce9b4917
Dominik Donoch <96125413+DDonochVA@users.noreply.github.com>
2026-07-20 22:21:36 +0200

    fix: youtube video 153 error on mobile (#551)

--- libs/blog/videos/ui-youtube-video-player/src/lib/components/youtube-video-player/youtube-video-player.component.ts
+++ libs/blog/videos/ui-youtube-video-player/src/lib/components/youtube-video-player/youtube-video-player.component.ts
@@ -1,9 +1,11 @@
+import { isPlatformBrowser } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   computed,
   inject,
   input,
+  PLATFORM_ID,
 } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -28,11 +30,18 @@ export class YoutubeVideoPlayerComponent {
     initialValue: this.translocoService.getActiveLang(),
   });
 
+  private readonly _origin = isPlatformBrowser(inject(PLATFORM_ID))
+    ? window.location.origin
+    : // ssr fallback
+      'https://angular.love';
+
   protected readonly videoSrc = computed(() => {
     const lang = encodeURIComponent(this.lang());
     const videoId = encodeURIComponent(this.videoId());
+    const origin = encodeURIComponent(this._origin);
+
     return this.domSanitizer.bypassSecurityTrustResourceUrl(
-      `https://www.youtube-nocookie.com/embed/${videoId}?rel=0&autoplay=1&hl=${lang}&cc_lang_pref=${lang}`,
+      `https://www.youtube-nocookie.com/embed/${videoId}?rel=0&autoplay=1&hl=${lang}&cc_lang_pref=${lang}&origin=${origin}`,
     );
   });
 }
